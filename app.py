8778eb54df6cab669e0ea4296d7362396426b32e
MariMatos2112 <maristudies2112@gmail.com>
2021-10-25 10:22:59 -0300

    form sending data

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        user_input = request.form.get('user_song')
+        return redirect(url_for('music_found', user_input=user_input))
+
     return render_template('index.html')
 
 @app.route('/music-found')
 def music_found():
+    user_input = request.args.get('user_input')
+    print(user_input)
     return render_template('music-found.html')
 
 if __name__ == '__main__':
