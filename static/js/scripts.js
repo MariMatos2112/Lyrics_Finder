2626e0b158089ca2511cec037d4be7029de3148e
MariMatos2112 <maristudies2112@gmail.com>
2021-10-27 10:05:39 -0300

    loading modal box added

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,9 @@
 const btnChangeBg = document.getElementById("changeBg");
 const divMainBox = document.getElementById("mainBox");
+const btnSubmit = document.querySelector("input[type=submit]");
+const divModalBox = document.getElementById("modal");
+const arrayClassAppear01 = document.querySelectorAll(".appear01");
+const arrayClassAppear02 = document.querySelectorAll(".appear02");
 
 const backgrounds = [
   "/static/images/undraw_mello.svg",
@@ -12,5 +16,18 @@ let counter = 0;
 btnChangeBg.addEventListener("click", () => {
   if (counter < backgrounds.length - 1) counter++;
   else counter = 0;
-  divMainBox.style.backgroundImage = `url(${backgrounds[counter]})`
+  divMainBox.style.backgroundImage = `url(${backgrounds[counter]})`;
+});
+
+btnSubmit.addEventListener("click", () => {
+  divModalBox.style.display = "flex";
+
+  setTimeout(() => {
+    arrayClassAppear01.forEach((element) => {
+      element.style.display = "none";
+    });
+    arrayClassAppear02.forEach((element) => {
+      element.style.display = "flex";
+    });
+  }, 4000);
 });
